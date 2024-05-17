c0a28f80433db9bbf2780f2ddb30a07e9b9a63f9
Lenka Zufala <m-lenkazufala@seznam.cz>
2024-05-17 17:11:59 +0200

    ukol

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-
+const goodbye = (name) => {
+    return 'S pozdravem ' + name;
+  };
+  
+  const fillSubject = (subject) => {
+    document.querySelector('.email__subject').textContent = subject;
+  };
+  
+  const fillBody = (body, name) => {
+    const bodyElm = document.querySelector('.email__body');
+    bodyElm.innerHTML += body;
+    const closingElm = document.querySelector('.email__closing');
+    closingElm.textContent = goodbye(name);
+  };
+  
+  fillSubject('Pozice FrontEnd vyvojarky');
+  fillBody(
+    "Budu se tesit na vasi odpoved","Lenka Zufala"
+  )
+  
